35b9809fe5a39ca750a7c44c45de16ede6a1948e
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-11-21 18:23:13 -0300

    feat(org): customizations

--- init.el
+++ init.el
@@ -561,6 +561,42 @@
   (proced-descent t)
   (proced-filter 'user)) ;; We can change interactively with `s'
 
+;;; ORG
+(use-package org
+  :ensure nil
+  :defer t
+  :mode ("\\.org\\'" . org-mode)
+  :config
+  (setq
+   ;; Start collapsed for speed
+   org-startup-folded t
+
+   ;; Edit settings
+   org-auto-align-tags nil
+   org-tags-column 0
+   org-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; Org styling, hide markup etc.
+   org-hide-emphasis-markers t
+   org-pretty-entities t
+
+   ;; Agenda styling
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "◀── now ─────────────────────────────────────────────────")
+
+  ;; Ellipsis styling
+  (setq org-ellipsis " ▼ ")
+  (set-face-attribute 'org-ellipsis nil :inherit 'default :box nil))
+
+
 ;;; RUBY-TS-MODE
 (use-package ruby-ts-mode
   :ensure nil
