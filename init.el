d75f125a0e1a15f81f061e5100c2f466a00e5c10
Rahul M. Juliato <rahul.juliato@gmail.com>
2024-11-24 03:44:11 -0300

    feat(highlight-keywords): new feature

--- init.el
+++ init.el
@@ -685,9 +685,63 @@ and restart Flymake to apply the changes."
   :bind (:map project-prefix-map
          ("P" . emacs-solo/find-projects-and-switch)))
 
+
+;;; EMACS-SOLO-HIGHLIGHT-KEYWORDS-MODE
+;;
+;;  Highlights a list of words like TODO, FIXME...
+;;  Code borrowed from `alternatived'
+;;
+(use-package emacs-solo-highlight-keywords-mode
+  :ensure nil
+  :defer t
+  :init
+  (defcustom +highlight-keywords-faces
+    '(("TODO" . error)
+      ("FIXME" . error)
+      ("HACK" . warning)
+      ("NOTE" . warning))
+    "Alist of keywords to highlight and their face."
+    :group '+highlight-keywords
+    :type '(alist :key-type (string :tag "Keyword")
+                  :value-type (symbol :tag "Face"))
+    :set (lambda (sym val)
+           (dolist (face (mapcar #'cdr val))
+             (unless (facep face)
+               (error "Invalid face: %s" face)))
+           (set-default sym val)))
+
+  (defvar +highlight-keywords--keywords
+    (when +highlight-keywords-faces
+      (let ((keywords (mapcar #'car +highlight-keywords-faces)))
+        `((,(regexp-opt keywords 'words)
+           (0 (when (nth 8 (syntax-ppss))
+                (cdr (assoc (match-string 0) +highlight-keywords-faces)))
+              prepend)))))
+    "Keywords and corresponding faces for `emacs-solo/highlight-keywords-mode'.")
+
+  (defun emacs-solo/highlight-keywords-mode-on ()
+    (font-lock-add-keywords nil +highlight-keywords--keywords t)
+    (font-lock-flush))
+
+  (defun emacs-solo/highlight-keywords-mode-off ()
+    (font-lock-remove-keywords nil +highlight-keywords--keywords)
+    (font-lock-flush))
+
+  (define-minor-mode emacs-solo/highlight-keywords-mode
+    "Highlight TODO and similar keywords in comments and strings."
+    :lighter " +HL"
+    :group '+highlight-keywords
+    (if emacs-solo/highlight-keywords-mode
+        (emacs-solo/highlight-keywords-mode-on)
+      (emacs-solo/highlight-keywords-mode-off)))
+
+  :hook
+  (prog-mode . (lambda () (run-at-time "1 sec" nil #'emacs-solo/highlight-keywords-mode-on))))
+
+
 ;;; EMACS-SOLO-GUTTER
 ;;
-;;  A **HIGHLY** `experimental' git gutter like.
+;;  A **HIGHLY** `experimental' and slow and buggy git gutter like.
 ;;
 (use-package emacs-solo-gutter
   :ensure nil
