d3b7547dc6506459e020886db3c9f569cf5c2c6e
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-03-01 01:09:52 -0300

    feat: ibuffer default filters

--- init.el
+++ init.el
@@ -84,6 +84,43 @@
   (set-display-table-slot standard-display-table 'vertical-border ?\u2502)
   (set-display-table-slot standard-display-table 'truncation ?\u2192)
 
+  ;; Ibuffer filters
+  (setq ibuffer-saved-filter-groups
+        '(("default"
+           ("org" (or
+                   (mode . org-mode)
+                   (name . "^\\*Org Src")
+                   (name . "^\\*Org Agenda\\*$")))
+           ("tramp" (name . "^\\*tramp.*"))
+           ("magit" (or
+                     (mode . magit-mode)
+                     (mode . magit-diff-mode)
+                     (mode . magit-process-mode)
+                     (name . "^magit.*")))
+           ("emacs" (or
+                     (name . "^\\*scratch\\*$")
+                     (name . "^\\*Messages\\*$")
+                     (name . "^\\*Warnings\\*$")
+                     (name . "^\\*Shell Command Output\\*$")
+                     (name . "^\\*Async-native-compile-log\\*$")
+                     (name . "^\\*straight-")))
+           ("ediff" (or
+                     (name . "^\\*ediff.*")
+                     (name . "^\\*Ediff.*")))
+           ("dired" (mode . dired-mode))
+           ("terminal" (or
+                        (mode . term-mode)
+                        (mode . shell-mode)
+                        (mode . eshell-mode)))
+           ("help" (or
+                    (name . "^\\*Help\\*$")
+                    (name . "^\\*info\\*$")
+                    (name . "^\\*helpful"))))))
+  (add-hook 'ibuffer-mode-hook
+            (lambda ()
+              (ibuffer-switch-to-saved-filter-groups "default")))
+  (setq ibuffer-show-empty-filter-groups nil) ;; don't show empty groups
+
   :init
   (set-window-margins (selected-window) 0 0)
 
