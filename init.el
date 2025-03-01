68607da2426b9591d8d454ab0990acf2ed2bdbaa
Rahul Martim Juliato <rahul.juliato@gmail.com>
2025-03-01 00:55:44 -0300

    feat: emacs-solo-sudo-edit

--- init.el
+++ init.el
@@ -1957,5 +1957,47 @@ Windows are labeled starting from the top-left window and proceeding top to bott
   ;; (add-hook 'newsticker-treeview-item-mode-hook 'emacs-solo/timed-center-visual-fill-on)
   )
 
+
+;; ---------- EMACS-SOLO-SUDO-EDIT
+;;
+;; Inspired by: https://codeberg.org/daviwil/dotfiles/src/branch/master/Emacs.org#headline-28
+(use-package emacs-solo-sudo-edit
+  :ensure nil
+  :no-require t
+  :defer t
+  :init
+  (defun emacs-solo/sudo-edit (&optional arg)
+    "Edit currently visited file as root.
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+    (interactive "P")
+    (if (or arg (not buffer-file-name))
+        (find-file (concat "/sudo:root@localhost:"
+                           (completing-read "Find file(as root): ")))
+      (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name)))))
+
+
+
+;; ---------- EMACS-SOLO-SUDO-EDIT
+;;
+;; Inspired by: https://codeberg.org/daviwil/dotfiles/src/branch/master/Emacs.org#headline-28
+(use-package emacs-solo-sudo-edit
+  :ensure nil
+  :no-require t
+  :defer t
+  :init
+  (defun emacs-solo/sudo-edit (&optional arg)
+    "Edit currently visited file as root.
+With a prefix ARG prompt for a file to visit.
+Will also prompt for a file to visit if current
+buffer is not visiting a file."
+    (interactive "P")
+    (if (or arg (not buffer-file-name))
+        (find-file (concat "/sudo:root@localhost:"
+                           (completing-read "Find file(as root): ")))
+      (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name)))))
+
+
 (provide 'init)
 ;;; init.el ends here
