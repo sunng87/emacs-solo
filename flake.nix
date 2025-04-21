e3e4102774fccef1c0beb8a50bb7e4d46255d97d
Ning Sun <sunning@greptime.com>
2025-04-21 11:23:36 +0800

    flake: udpate flake options

--- flake.nix
+++ flake.nix
@@ -10,15 +10,30 @@
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
   in {
-    homeManagerModules.default = { pkgs, lib, ... }: {
-      home.file.".emacs-solo/init.el".source = ./init.el;
-      home.file.".emacs-solo/early-init.el".source = ./early-init.el;
+    homeManagerModules.default = { pkgs, lib, config, ... }:
+    let
+      cfg = config.programs.emacs-solo;
+    in {
+      options.programs.emacs-solo = {
+        enable = lib.mkEnableOption "emacs-solo configuration";
 
-      home.packages = [
-        (pkgs.writeShellScriptBin "e" ''
-          exec ${pkgs.emacs}/bin/emacs --init-directory="$HOME/.emacs-solo" "$@"
-        '')
-      ];
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = pkgs.emacs;
+          description = "The Emacs package to use";
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        home.file.".emacs-solo/init.el".source = ./init.el;
+        home.file.".emacs-solo/early-init.el".source = ./early-init.el;
+
+        home.packages = [
+          (pkgs.writeShellScriptBin "e" ''
+            exec ${cfg.package}/bin/emacs --init-directory="$HOME/.emacs-solo" "$@"
+          '')
+        ];
+      };
     };
   };
 }
